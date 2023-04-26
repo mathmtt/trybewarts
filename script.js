cbeab84b733a219ddbe37339a56354da434f5f26
Gabriel Moraes <gabriel_lmoraes@outlook.com>
2023-04-26 18:43:08 -0300

    Exercico 18

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Exercicio 3
+
 const mail = document.getElementById('email');
 const password = document.getElementById('password');
 const loginBtn = document.getElementById('btn-login');
@@ -10,3 +12,17 @@ function access() {
   }
 }
 loginBtn.addEventListener('click', access);
+
+//Exercicio 18
+
+const btnSubmit = document.querySelector('#submit-btn');
+const agreement = document.querySelector('#agreement');
+
+btnSubmit.disabled = true;
+agreement.addEventListener('click', () => {
+  if (agreement.checked) {
+    btnSubmit.disabled = false;
+  } else {
+    btnSubmit.disabled = true;
+  }
+});
