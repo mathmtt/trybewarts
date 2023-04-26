3b554e4a903b9d4ca4d1397f2336f828c886c24b
Matheus Motta <matheusmotta136@gmail.com>
2023-04-26 16:04:47 -0300

    fazendo o scrpit do botao

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+function access() {
+    if (mail.value === 'tryber@teste.com' && password.value === '123456') {
+       alert('Olá, Tryber!'); 
+    } else {
+        alert ('Email ou senha inválidos.');
+    }
+}
+btnLogin.addEventListener('click', access);
